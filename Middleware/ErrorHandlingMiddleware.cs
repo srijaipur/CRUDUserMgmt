730abea0fa1d76b637f67b3c315c8cb8f347883f
Srinivas SP <srijaipur@gmail.com>
2025-04-04 23:20:43 -0400

    Create ErrorHandlingMiddleware.cs

---
+++ Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,23 @@
+public class ErrorHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = 500;
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsJsonAsync(new { error = ex.Message });
+        }
+    }
+}
